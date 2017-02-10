b4bad2e0256c122c6321b9c319aee2d1b9eecc68
Marc Joseph Streeter <marc.joseph.streeter@gmail.com>
2017-02-09 18:44:01 -0600

    Initial Commit

---
+++ SnapChat3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapChat3
+//
+//  Created by Marc Streeter on 2/9/17.
+//  Copyright © 2017 Developer Warriors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
